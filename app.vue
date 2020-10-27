eecef631ac56eb15afe3b9cb3097aa53603d6583
HoshinoNia <70749961+HoshinoNia@users.noreply.github.com>
2020-10-28 00:17:02 +0900

    initial

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <h1>hello!</h1>
+    <p>message...</p>
+
+    <h2>チェック</h2>
+    <h2>チェック</h2>
+    <h2>チェック</h2>
+    <h2>チェック</h2>
+  </div>
+</template>
+
+<script>
+</script>
